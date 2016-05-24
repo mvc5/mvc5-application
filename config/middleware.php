cd7fb011026e06edd34fa8e7a0b55676a1cad0b7
devosc <devosc@gmail.com>
2016-05-24 01:04:55 -0500

    controller\dispatch, web\dispatch (short circuit)

--- config/middleware.php
+++ config/middleware.php
@@ -8,11 +8,14 @@ return [
         'web\route',
         'web\error',
         'web\service',
-        'web\controller',
-        'web\layout',
-        'web\render',
+        'web\dispatch',
         'web\status',
         'web\version',
         'web\send',
-    ]
+    ],
+    'web\response' => [
+        'web\controller',
+        'web\layout',
+        'web\render',
+    ],
 ];
