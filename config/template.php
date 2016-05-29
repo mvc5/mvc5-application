3d8ec084447f1969210cbc555e698b6d986e2ca7
devosc <devosc@gmail.com>
2016-05-28 22:19:55 -0500

    rename error and exception templates

--- config/template.php
+++ config/template.php
@@ -4,10 +4,10 @@
  */
 
 return [
-    //'blog'            => __DIR__ . '/../view/blog/index.phtml',
-    'blog:create'     => __DIR__ . '/../view/blog/create.phtml',
-    'error/exception' => __DIR__ . '/../vendor/mvc5/mvc5/view/exception.phtml',
-    'error/error'       => __DIR__ . '/../view/error/error.phtml',
-    'home'            => __DIR__ . '/../view/home/index.phtml',
-    'layout'          => __DIR__ . '/../view/layout/layout.phtml',
+    //'blog'        => __DIR__ . '/../view/blog/index.phtml',
+    'blog:create' => __DIR__ . '/../view/blog/create.phtml',
+    'exception'   => __DIR__ . '/../vendor/mvc5/mvc5/view/exception.phtml',
+    'error'       => __DIR__ . '/../view/error/error.phtml',
+    'home'        => __DIR__ . '/../view/home/index.phtml',
+    'layout'      => __DIR__ . '/../view/layout/layout.phtml',
 ];
