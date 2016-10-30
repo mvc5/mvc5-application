c21fc590157530559486548c922aaeb072805e85
devosc <devosc@gmail.com>
2016-10-29 20:47:25 -0500

    overview page uses view render service plugin

---
+++ src/Overview/Controller.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ */
+
+namespace Overview;
+
+use Mvc5\Model;
+use Mvc5\Model\ViewLayout;
+use Mvc5\Plugins\Messages;
+use Mvc5\Plugins\Render;
+use Mvc5\Plugins\Service;
+use Mvc5\Plugins\ViewModel;
+use Request;
+
+class Controller
+{
+    /**
+     *
+     */
+    use Messages;
+    use Render;
+    use Service;
+    use ViewModel;
+
+    /**
+     *
+     */
+    const VIEW_MODEL = Model::class;
+
+    /**
+     *
+     */
+    const TEMPLATE_NAME = 'overview/index';
+
+    /**
+     * @param Request $request
+     * @param ViewLayout $layout
+     * @return string
+     */
+    function __invoke(Request $request, ViewLayout $layout)
+    {
+        $this->warning('Documentation is maintained at <a href="http://mvc5.github.io">http://mvc5.github.io</a>');
+
+        $layout->model($this->model(['request' => $request]));
+
+        return $this->render($layout);
+    }
+}
