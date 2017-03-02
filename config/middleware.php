3de56195c0d18f36ff597449bb556038d2e53873
devosc <devosc@gmail.com>
2017-03-02 07:41:37 -0600

    router dependencies, route match middleware pattern

--- config/middleware.php
+++ config/middleware.php
@@ -4,6 +4,17 @@
  */
 
 return [
+    'route\match' => new Mvc5\Plugin\Args([
+        new Mvc5\Plugin\Plugin('route\match\merge'),
+        new Mvc5\Plugin\Plugin('route\match\scheme'),
+        new Mvc5\Plugin\Plugin('route\match\host'),
+        new Mvc5\Plugin\Plugin('route\match\path'),
+        new Mvc5\Plugin\Plugin('route\match\method'),
+        new Mvc5\Plugin\Plugin('route\match\action'),
+        new Mvc5\Plugin\Plugin('route\match\controller'),
+        new Mvc5\Plugin\Plugin('route\match\middleware'),
+        new Mvc5\Plugin\Plugin('route\match\wildcard')
+    ]),
     'web' => [
         'web\context',
         'web\route',
