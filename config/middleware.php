f5ecc89375d0355a4ac409c11d83fdf3316bc1d1
devosc <devosc@gmail.com>
2017-03-03 07:29:03 -0600

    tidy up

--- config/middleware.php
+++ config/middleware.php
@@ -3,17 +3,20 @@
  *
  */
 
+use Mvc5\Plugin\Args;
+use Mvc5\Plugin\Plugin;
+
 return [
-    'route\match' => new Mvc5\Plugin\Args([
-        new Mvc5\Plugin\Plugin('route\match\merge'),
-        new Mvc5\Plugin\Plugin('route\match\scheme'),
-        new Mvc5\Plugin\Plugin('route\match\host'),
-        new Mvc5\Plugin\Plugin('route\match\path'),
-        new Mvc5\Plugin\Plugin('route\match\method'),
-        new Mvc5\Plugin\Plugin('route\match\action'),
-        new Mvc5\Plugin\Plugin('route\match\controller'),
-        new Mvc5\Plugin\Plugin('route\match\middleware'),
-        new Mvc5\Plugin\Plugin('route\match\wildcard')
+    'route\match' => new Args([
+        new Plugin('route\match\merge'),
+        new Plugin('route\match\scheme'),
+        new Plugin('route\match\host'),
+        new Plugin('route\match\path'),
+        new Plugin('route\match\method'),
+        new Plugin('route\match\action'),
+        new Plugin('route\match\controller'),
+        new Plugin('route\match\middleware'),
+        new Plugin('route\match\wildcard')
     ]),
     'web' => [
         'web\context',
