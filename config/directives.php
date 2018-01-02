312699f1dc6d54940ac3a330dfdbcda12d408d16
devosc <devosc@gmail.com>
2018-01-01 22:59:02 -0600

    expression tokens

--- config/directives.php
+++ config/directives.php
@@ -1,14 +1,19 @@
 <?php
 /**
- *
+ * Available Tokens:
+ *  {expression} includes parentheses, e.g ('user')
+ *  {expr} without parentheses, e.g 'user'
+ *  {var} variable name, e.g user
  */
 
 use View5\Template;
+use View5\Token\Expression as Expr;
 
 return [
     'auth' => '<?php ${var} = $this->plugin({expr}); if (${var}->authenticated()) : ?>',
     'endAuth' => '<?php endif; ?>',
-    /*'endAuth' => function(string $expression, Template $template) : string {
-        return '<?php endif; ?>';
+    /*'auth' => function(string $expression, Template $template) : string {
+        $var = Expr::var($expression); $plugin = Expr::expr($expression);
+        return '<?php $' . $var . ' = $this->plugin(' . $plugin . '); if ($' . $var . '->authenticated()) : ?>';
     },*/
 ];
