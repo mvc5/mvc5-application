2b9cb7d7353917e5ebc1b5c20e38755974c44bc5
devosc <devosc@gmail.com>
2018-06-11 23:14:03 -0500

    service parameter

--- src/Plugin/Page.php
+++ src/Plugin/Page.php
@@ -8,7 +8,8 @@ namespace Plugin;
 use Mvc5\Arg;
 use Mvc5\Http\Request;
 use Mvc5\Plugin\Call;
-use Mvc5\Plugin\Callback;
+use Mvc5\Plugin\Link;
+use Mvc5\Service\Service;
 
 class Page
     extends Call
@@ -19,18 +20,19 @@ class Page
      */
     function __construct(string $template, array $vars = [])
     {
-        parent::__construct([$this, '__invoke'], [$template, $vars]);
+        parent::__construct([$this, '__invoke'], [new Link, $template, $vars]);
     }
 
     /**
+     * @param Service $service
      * @param string $template
      * @param array $vars
-     * @return Callback
+     * @return \Closure
      */
-    function __invoke(string $template, array $vars)
+    function __invoke(Service $service, string $template, array $vars) : \Closure
     {
-        return new Callback(function(Request $request) use($template, $vars) {
-            return $this->plugin(Arg::VIEW_MODEL, [$template, $vars + [Arg::REQUEST => $request]]);
-        });
+        return function(Request $request) use($service, $template, $vars) {
+            return $service->plugin(Arg::VIEW_MODEL, [$template, $vars + [Arg::REQUEST => $request]]);
+        };
     }
 }
